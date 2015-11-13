c0d0f92f1e579d197867142c9c8be662d1289ec2
tlsion <pro@tingxiedeMac-mini.local>
2015-11-13 18:06:48 +0800

    1
    1

---
+++ Meiye/AppFoundation/Controllers/LoginHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginHomeViewController.h
+//  Meiye
+//
+//  Created by tingxie on 15/11/13.
+//  Copyright © 2015年 王庭协. All rights reserved.
+//
+
+#import "TXBaseViewController.h"
+
+@interface LoginHomeViewController : TXBaseViewController
+
+@end
---
+++ Meiye/AppFoundation/Controllers/LoginHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginHomeViewController.m
+//  Meiye
+//
+//  Created by tingxie on 15/11/13.
+//  Copyright © 2015年 王庭协. All rights reserved.
+//
+
+#import "LoginHomeViewController.h"
+
+@interface LoginHomeViewController ()
+
+@end
+
+@implementation LoginHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
